d71325f9bbadfc81d8bfd1af9abcc5b0542573aa
Leekj1313 <153248086+Leekj1313@users.noreply.github.com>
2023-12-08 17:42:04 +0900

    자바 1회차 연습문제

---
+++ Java_JV/src/day01/TestEx1.java
@@ -0,0 +1,15 @@
+package day01;
+
+public class TestEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// 문제1.
+		char ch1 = 'A';
+		System.out.println(ch1);
+		
+		
+	}
+
+}
---
+++ Java_JV/src/day01/TestEx2.java
@@ -0,0 +1,19 @@
+package day01;
+
+public class TestEx2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//문제 2.국어,영어 수학의 평균
+		// 3과목의 점수를 어떤 값으로 이용할 것인가? 정수 아님 소수점 득점기준
+		int kor1, eng1, math1;
+		//정수
+		double kor2, eng2, math2;
+		//소수
+		double kor2, eng2, math2;
+		double avg;
+		//평균
+	}
+
+}
