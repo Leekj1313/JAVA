25b7a4269c942700f247f2faa8decd071b83ed06
Leekj1313 <153248086+Leekj1313@users.noreply.github.com>
2023-12-28 12:50:02 +0900

    Day14Am

--- Java_JV/src/Day14/ArithmeticExceptionEx2.java
+++ Java_JV/src/Day14/ArithmeticExceptionEx2.java
@@ -1,6 +1,7 @@
 package Day14;
 
 import java.security.PublicKey;
+import java.text.MessageFormat;
 import java.util.Scanner;
 
 public class ArithmeticExceptionEx2 {
@@ -12,28 +13,37 @@ public class ArithmeticExceptionEx2 {
 		
 		Scanner scan = new Scanner(System.in);
 		System.out.println("두 정수와 산술 연산자를 입력하시오 : ");
-		printOpr();
 		try {
-			//printOpr();
-		}//catch (Exception e) {
-			//System.out.println("에외를 처리했습니다.");
-		//}
-	public static void printOpr() {
-		int num1 = scan.nextInt();
-		int num2 = scan.nextInt();
-		char op	 = scan.next().charAt(0);
-		double res = 0;
+			int num1 = scan.nextInt();
+			int num2 = scan.nextInt();
+			char op = scan.next().charAt(0);
+			double res = 0;
+			String pattern = "{0} {1} {2} = {3}";
+			System.out.println(MessageFormat.format(pattern, num1, op, num2, res));
 		
-		switch(op) {
-		case '+' : res = num1 + num2; break;
-		case '-' : res = num1 - num2; break;
-		case '*' : res = num1 * num2; break;
-		case '/' : res = num1 / num2; break;
-		case '%' : res = num1 % num2; break;
+			
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+		public static double double Caculator(int num1, char op, int num2) {
+			//throws RumtimeException  // 생략가능 
+			switch(op) {
+			case '+':	return num1 + num2;
+			case '-':	return num1 - num2;
+			case '*':	return num1 * num2;
+			case '%':	return num1 % num2;
+				if(num2 == 0) {
+					throw new ArithmeticException("0으로 나눌 수 없습니다.");
+				}
+				return num1 % num2;
+			case '/':	return num1 / (double)num2;
+				if(num2 == 0) {
+					throw new ArithmeticException("0으로 나눌 수 없습니다.");
+			default:
+					throw new RuntimeException(op + "는 산술 연산자가 아닙니다.");
+					}
+			}
 		}
 		
-
-	}
-
-	}
 
